0e64ac49ae1bb69c4f66ce2cdda902777fc4206d
devsfer1 <fernando.chaves16@hotmail.com>
2021-08-26 06:52:52 -0300

    feat: creating category

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
+/* eslint-disable import/no-unresolved */
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => response.json({ message: 'Hello Word' }));
-
-app.post('/courses', (request, response) => {
-  const { name } = request.body;
-  return response.json({ name });
-});
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log('Servidor Rodando'));
