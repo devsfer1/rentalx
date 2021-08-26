b6104768c44140f40869c6aebb5ff5a1e1694a53
devsfer1 <fernando.chaves16@hotmail.com>
2021-08-26 06:36:07 -0300

    debugging code

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,13 @@ import express from 'express';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello Word"})
-})
+app.use(express.json());
+
+app.get('/', (request, response) => response.json({ message: 'Hello Word' }));
+
+app.post('/courses', (request, response) => {
+  const { name } = request.body;
+  return response.json({ name });
+});
 
 app.listen(3333, () => console.log('Servidor Rodando'));
